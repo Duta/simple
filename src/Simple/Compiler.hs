565cb4eacfdcdde292198d4b68c838ddf1d12975
Bertie Wheen <wheen.b@gmail.com>
2014-06-12 17:35:14 +0100

    Compile initialization

--- src/Simple/Compiler.hs
+++ src/Simple/Compiler.hs
@@ -3,10 +3,22 @@ module Simple.Compiler where
 import           Simple.AST
 import           Simple.VM
 
-compile :: Stmt -> Bytecode
-compile (Seq stmts)                 = concatMap compile stmts
-compile (While cond stmts)          = undefined
-compile (IfElse cond stmts1 stmts2) = undefined
-compile (If cond stmts)             = undefined
-compile (Init varType var expr)     = undefined
-compile (Expr expr)                 = undefined
+class Compilable a where
+  compile :: a -> Bytecode
+
+instance Compilable Stmt where
+  compile (Seq stmts)                 = concatMap compile stmts
+  compile (While cond stmts)          = undefined
+  compile (IfElse cond stmts1 stmts2) = undefined
+  compile (If cond stmts)             = undefined
+  compile (Init varType var expr)     = compile $ Set var expr
+  compile (Expr expr)                 = undefined
+
+instance Compilable Expr where
+  compile (Set var expr)       = undefined
+  compile (FuncCall func args) = undefined
+  compile (Var var)            = undefined
+  compile (IntLit int)         = undefined
+  compile (BoolLit bool)       = undefined
+  compile (UnaryOp op expr)    = undefined
+  compile (BinaryOp op e1 e2)  = undefined
