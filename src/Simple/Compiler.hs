9c8237ebd25ae469300b2cf44aaad1a119c47ba2
Bertie Wheen <wheen.b@gmail.com>
2014-06-12 17:27:31 +0100

    Base compiler

--- src/Simple/Compiler.hs
+++ src/Simple/Compiler.hs
@@ -1,3 +1,12 @@
 module Simple.Compiler where
 
-compile = undefined
+import           Simple.AST
+import           Simple.VM
+
+compile :: Stmt -> Bytecode
+compile (Seq stmts)                 = undefined
+compile (While cond stmts)          = undefined
+compile (IfElse cond stmts1 stmts2) = undefined
+compile (If cond stmts)             = undefined
+compile (Init varType var expr)     = undefined
+compile (Expr expr)                 = undefined
