38c596c8c2711a6afd624faebdd6c9ad0a3da654
Bertie Wheen <wheen.b@gmail.com>
2014-06-12 19:25:46 +0100

    Main script now executes

--- src/main.hs
+++ src/main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Simple.Parser (parseFile)
 import Simple.Compiler (compile)
+import Simple.VM (execute)
 import System.Environment (getArgs)
 
-main = getArgs >>= mapM_ ((>>= print . compile) . parseFile)
+main = getArgs >>= mapM_ ((>>= execute . compile) . parseFile)
